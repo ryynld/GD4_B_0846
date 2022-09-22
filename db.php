85458e7c877266b9fb340777ae30223440f09c6f
ryynld <henareynald@gmail.com>
2022-09-22 23:26:05 +0700

    First Commit

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+    $host = "localhost"; 
+    $user = "root"; 
+    $pass = ""; 
+    $name = "GD4_B_0846"; 
+    $con = mysqli_connect($host,$user,$pass,$name); 
+    
+    if(mysqli_connect_errno()){ 
+        echo "Failed to connect : " . mysqli_connect_error(); 
+    } 
+?>
---
+++ process/registerProcess.php
@@ -0,0 +1,34 @@
+<?php
+    if(isset($_POST['register'])){
+        include('../db.php');
+        $email = $_POST['email']; 
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT); 
+        $name = $_POST['name']; 
+        $phonenum = $_POST['phonenum']; 
+        $membership = $_POST['membership'];
+
+        $query = mysqli_query($con,
+        "INSERT INTO users(email, password, name, phonenum, membership) 
+            VALUES
+            ('$email', '$password', '$name', '$phonenum', '$membership')") or die(mysqli_error($con)); 
+        
+        if($query){
+            echo
+                '<script>
+                    alert("Register Success"); 
+                    window.location = "../index.php" 
+                </script>';
+        }else{
+            echo
+            '<script>
+                alert("Register Failed");
+            </script>';
+        }
+    }else{
+        echo
+            '<script>
+                window.history.back()
+            </script>';
+    }
+?>
+        
